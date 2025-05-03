eb84d8f7d22f8c97a915d2236b24aa18a5dabb72
Devil <dasblueeyeddevil@gmail.com>
2025-05-03 19:01:56 -0400

    test

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /home/aydocorp/public_html
-/usr/bin/node server.js >> app.log 2>&1
+# Change to the directory containing server.js
+cd /home/aydocorp/public_html/nodejs/aydocorp-api/ >> app.log 2>&1
+# Run the Node.js server and redirect output to app.log
+node server.js >> app.log 2>&1
