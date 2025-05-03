87f9326751ca211a174603e11bb7ddfb9944b262
Devil <dasblueeyeddevil@gmail.com>
2025-05-03 14:24:19 -0400

    creating server restart func

--- nodejs/aydocorp-api/monitor.sh
+++ nodejs/aydocorp-api/monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set path to your Node.js app
-APP_DIR=~/public_html/nodejs/aydocorp-api
+APP_DIR=/home/aydocorp/public_html/nodejs/aydocorp-api
 LOG_FILE=$APP_DIR/app.log
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
@@ -12,12 +12,11 @@ then
 else
     echo "[$TIMESTAMP] Node.js process is not running. Restarting..." >> $APP_DIR/monitor.log
     
-    # Kill any zombie processes if they exist
-    pkill -f "node server.js" >> $APP_DIR/monitor.log 2>&1
-    
-    # Start Node.js process
+    # Change to the app directory
     cd $APP_DIR
-    nohup node server.js > $LOG_FILE 2>&1 &
     
-    echo "[$TIMESTAMP] Node.js process restarted with PID: $!" >> $APP_DIR/monitor.log
+    # Restart using npm
+    npm restart >> $APP_DIR/monitor.log 2>&1
+    
+    echo "[$TIMESTAMP] Node.js process restarted via npm restart" >> $APP_DIR/monitor.log
 fi
